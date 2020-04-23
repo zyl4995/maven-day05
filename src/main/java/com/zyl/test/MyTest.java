55f67a4d4490617cade211d69c3cd63775e6830a
zyl <zyl@qq.com>
2020-04-23 09:36:09 +0800

    111

--- src/main/java/com/zyl/test/MyTest.java
+++ src/main/java/com/zyl/test/MyTest.java
@@ -4,6 +4,6 @@ public class MyTest {
 
 //	（二）编写main方法，输出helloWord。（10分）
 	public static void main(String[] args) {
-		System.out.println("helloWord");
+		System.out.println("helloWord11111");
 	}
 }
